0427ef0d4dc1dc30e084e2047b71151ecea80f8f
Ktosox <ktoso3@gmail.com>
2023-08-14 23:35:11 +0200

    Made a cool dragable boulder

--- Game/Utility/Draggy.gd
+++ Game/Utility/Draggy.gd
@@ -4,11 +4,28 @@ signal dragging(state)
 
 var drag = false
 
+export var dragRange = 150.0 # max distance between draggy and dragged object
+
+
+func start_drag():
+	drag = true
+	emit_signal("dragging",true)
+	$GrabbyPart/Line2D.visible = true
+	pass
+
+func end_drag():
+	emit_signal("dragging",false)
+	drag = false
+	$GrabbyPart/Line2D.visible = false
+	pass
+
 
 func _physics_process(delta):
 	
 	if drag:
 		$GrabbyPart.global_position = get_global_mouse_position()
+		if $GrabbyPart.global_position.distance_to(get_parent().global_position) > dragRange :
+			end_drag()
 	else:
 		global_rotation = 0
 		$GrabbyPart.global_position = global_position
@@ -16,12 +33,10 @@ func _physics_process(delta):
 func _input(event):
 	if event.is_class("InputEventMouseButton") and drag:
 		if event.button_index == 1 and !event.is_pressed():
-			emit_signal("dragging",false)
-			drag = false
+			end_drag()
 			
 
 func _on_GrabbyPart_input_event(viewport, event:InputEvent, shape_idx):
 	if event.is_class("InputEventMouseButton") and !drag:
 		if event.button_index == 1 and event.is_pressed():
-			drag = true
-			emit_signal("dragging",true)
+			start_drag()
